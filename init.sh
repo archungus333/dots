64618cd5ad320865348885b98a4463f967a4eba4
whatev3r <113145908+archungus333@users.noreply.github.com>
2022-12-01 12:12:47 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ sudo rm -f strap.sh
 # Package Sync & Init Installation
 sudo pacman -Syyu --noconfirm
 # Env Installation
-sudo pacman -S --noconfirm --needed bspwm sxhkd polybar picom nitrogen kitty lightdm lightdm-gtk-greeter dmenu xorg xorg-utils xorg-drivers xorg-apps
+sudo pacman -S --noconfirm --needed bspwm sxhkd polybar picom nitrogen kitty lightdm lightdm-gtk-greeter dmenu xorg xorg-utils xorg-drivers xorg-apps xorg-xf86-video-intel
 # Misc Installation
 sudo pacman -S --noconfirm --needed nano neovim vim htop tree neofetch cmatrix python python-pip python-pywal noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-font-awesome 
 
