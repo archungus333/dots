56103197ffe9bb861d5f9c67351308eed0be67d3
whatev3r <113145908+archungus333@users.noreply.github.com>
2022-12-07 16:10:15 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -16,21 +16,21 @@ sudo pacman -Syyu --noconfirm
 # Env Installation
 sudo pacman -S --noconfirm --needed bspwm sxhkd polybar picom nitrogen kitty lightdm lightdm-gtk-greeter dmenu xorg xorg-utils xorg-drivers xorg-apps xorg-xf86-video-intel
 # Misc Installation
-sudo pacman -S --noconfirm --needed nano neovim vim emacs htop btop tree neofetch cmatrix python python-pip python-pywal noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-font-awesome 
+sudo pacman -S --noconfirm --needed nano neovim vim emacs htop btop tree unzip neofetch cmatrix python python-pip python-pywal noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-font-awesome 
 
 # YAY Setup
 git clone https://aur.archlinux.org/yay.git /tmp/yay
 cd /tmp/yay && makepkg -si --noconfirm && cd $HOME
 # YAY Init Installation
-yay -S picom-ibhagwan-git
-yay -S nerd-fonts-complete
+yay -S --noconfirm picom-ibhagwan-git
+yay -S --noconfirm nerd-fonts-complete
 
 # Spotify Setup
-yay -S --noconfirm spotify spicetify-cli
-curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
-sudo chmod +x /opt/spotify
-sudo chmod +x /opt/spotify/Apps -R
-spicetify backup apply
+# yay -S --noconfirm spotify spicetify-cli
+# curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
+# sudo chmod +x /opt/spotify
+# sudo chmod +x /opt/spotify/Apps -R
+# spicetify backup apply
 
 # Lightdm Setup
 sudo echo 'Section "InputClass"
@@ -42,12 +42,12 @@ EndSection' > /etc/X11/xorg.conf.d/20-keyboard.conf
 sudo systemctl enable lightdm.service
 
 # Move Dots
-git clone https://www.github.com/archungus333/dots.git /tmp/dots
+git clone https://www.github.com/archungus333/dots.git /tmp/dots 
 cp -r /tmp/dots/configs/* $HOME/.config
 cp -r /tmp/dots/wallpapers $HOME
 
 # Exec Privs
-sudo chmod +x $HOME/.config/bspwmrc
+sudo chmod +x $HOME/.config/bspwm/bspwmrc
 sudo chmod +x $HOME/.config/autostart/*
 
 # User Privs
