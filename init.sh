0a5d34794ec19982ea91eceda02c6f7a29992f02
whatev3r <113145908+archungus333@users.noreply.github.com>
2022-12-07 15:26:16 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Username Input
-echo "[Make sure init.sh is executed as user & located in $home/dots!]"
-read -p "[Enter Username]: " user
-
 # Black Arch Repo Sync
 sudo curl -O https://blackarch.org/strap.sh
 sudo echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
@@ -17,6 +13,20 @@ sudo pacman -S --noconfirm --needed bspwm sxhkd polybar picom nitrogen kitty lig
 # Misc Installation
 sudo pacman -S --noconfirm --needed nano neovim vim htop tree neofetch cmatrix python python-pip python-pywal noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-font-awesome 
 
+# YAY
+git clone https://aur.archlinux.org/yay.git /opt
+cd /opt/yay && makepkg -si --noconfirm && cd $HOME && rm -rf /opt/yay
+# Init YAY Installs
+yay -S picom-ibhagwan-git
+yay -S nerd-fonts-complete
+
+# Spotify Setup
+yay -S --noconfirm spotify spicetify-cli
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
+sudo chmod +x /opt/spotify
+sudo chmod +x /opt/spotify/Apps -R
+spicetify backup apply
+
 # Lightdm Keyboard layout
 sudo echo 'Section "InputClass"
     Identifier "keyboard"
@@ -24,32 +34,18 @@ sudo echo 'Section "InputClass"
     Option "XkbLayout" "de"
     Option "XkbVariant" "nodeadkeys"
 EndSection' > /etc/X11/xorg.conf.d/20-keyboard.conf
-
 # Systemd Symlinks
 sudo systemctl enable lightdm.service
 
-# User Privs
-sudo chown -Rv $user:$user /home/$user
-
-# Move Configs
-cp -r configs/* $HOME/.config
-cp -r wallpapers $HOME
+# Move dots
+git clone https://www.github.com/archungus333/dots.git /opt
+cp -r /opt/dots/configs/* $HOME/.config
+cp -r /opt/dots/wallpapers $HOME
+rm -rf /opt/dots
 
 # Exec Privs
 sudo chmod +x $HOME/.config/bspwmrc
 sudo chmod +x $HOME/.config/autostart/*
 
-# YAY
-git clone https://aur.archlinux.org/yay.git /opt
-cd /opt/yay && makepkg -si --noconfirm && cd $HOME && rm -rf /opt/yay
-
-# Init YAY Installs
-yay -S picom-ibhagwan-git
-yay -S nerd-fonts-complete
-
-# Spotify
-yay -S --noconfirm spotify spicetify-cli
-curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
-sudo chmod +x /opt/spotify
-sudo chmod +x /opt/spotify/Apps -R
-spicetify backup apply
+# User Privs
+sudo chown -Rv $user:$user /home/$user
