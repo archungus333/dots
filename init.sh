798af2e903fd8fa462fd900d357d00a8ceecb563
whatev3r <113145908+archungus333@users.noreply.github.com>
2022-12-07 16:46:01 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -24,7 +24,7 @@ cd /tmp/yay && makepkg -si --noconfirm && cd $HOME
 # YAY Init Installation
 yay -S --noconfirm picom-ibhagwan-git
 yay -S --noconfirm nerd-fonts-complete
-yay -S --noconfirm spotify
+# yay -S --noconfirm spotify
 
 # Spotify Setup
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
